f3fabe932e810e784f30f43a83ec48dc8fb095af
Sala <Salambaye2001@gmail.com>
2024-07-15 17:41:58 +0200

    modif

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -4,9 +4,10 @@ const About = () => {
   return (
     <div>
       <h1>A propos</h1>
-      <p>Ce projet a été créé pour démontrer les fonctionnalités de MUI.</p>
-      <p>Il utilise les composants de MUI pour créer une interface utilisateur intuitive et attractive.</p>
-      <p>L'API utilisé est la suivante : https://jsonplaceholder.typicode.com/posts.</p>
+      <p>Ce projet démontre les fonctionnalités avancées de MUI (Material-UI) pour la création d'une interface utilisateur moderne et intuitive.</p>
+        <p>L'application utilise React et TypeScript avec Vite pour le bundling, offrant ainsi une structure optimisée et performante.</p> 
+        <p>L'accent est mis sur la gestion élégante des composants graphiques et sur l'intégration fluide avec une API externe, choisie librement pour ce projet.</p> 
+        <p>En mettant en œuvre des hooks personnalisés, nous améliorons l'efficacité du code et facilitons la gestion des états et des effets asynchrones, assurant une expérience utilisateur robuste et agréable.</p>
     </div>
   );
 };
